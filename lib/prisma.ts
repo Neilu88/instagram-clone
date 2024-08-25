62ccf51401b0d7431514bbb13de5e9b7dafc8468
Neilu88 <105619905+Neilu88@users.noreply.github.com>
2024-08-25 17:13:55 -0400

    added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // Prevent multiple PrismaClient instances in development
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // Optional: Adjust logging levels as needed
+    log: ["query", "info", "warn", "error"],
+  });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
