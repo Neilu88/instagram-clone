e07834d989dccf21b27c458055d0664272e94141
Neilu88 <105619905+Neilu88@users.noreply.github.com>
2024-10-29 01:37:37 -0400

    fixed postview

--- components/PostView.tsx
+++ components/PostView.tsx
@@ -1,9 +1,11 @@
 "use client";
+
+import CommentForm from "@/components/CommentForm";
 import PostActions from "@/components/PostActions";
 import UserAvatar from "@/components/UserAvatar";
-//import ViewPost from "@/components/ViewPost";
+import ViewPost from "@/components/ViewPost";
 import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
-//import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import useMount from "@/hooks/useMount";
 import { PostWithExtras } from "@/lib/definitions";
 import { useSession } from "next-auth/react";
@@ -11,18 +13,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useRef } from "react";
-import { ScrollArea } from "./ui/scroll-area";
 import MiniPost from "./MiniPost";
 import Comment from "./Comment";
-import ViewPost from "./ViewPost";
-import CommentForm from "./CommentForm";
-//import MiniPost from "./MiniPost";
-//import Comment from "./Comment";
 // import MiniPost from "./MiniPost";
 
 function PostView({ id, post }: { id: string; post: PostWithExtras }) {
   const pathname = usePathname();
-  const isPostModal = pathname == `/dashboard/p/${id}`;
+  const isPostModal = pathname === `/dashboard/p/${id}`;
   const router = useRouter();
   const { data: session, status } = useSession();
   const user = session?.user;
@@ -34,8 +31,8 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
   if (!mount) return null;
 
   return (
-    <Dialog open={isPostModal} onOpenChange={(open) => !open && router.back}>
-      <DialogContent className="flex gap-o flex-col md:flex-row items-start p-0 md:max-w-3xl lg:max-w-5xl xl:max-w-6xl h-full max-h-[500px] lg:max-h-[700px] xl:max-h-[800px]">
+    <Dialog open={isPostModal} onOpenChange={(open) => !open && router.back()}>
+      <DialogContent className="flex gap-0 flex-col md:flex-row items-start p-0 md:max-w-3xl lg:max-w-5xl xl:max-w-6xl h-full max-h-[500px] lg:max-h-[700px] xl:max-h-[800px]">
         <div className="flex flex-col justify-between md:h-full md:order-2 w-full max-w-md">
           <DialogHeader className="flex border-b space-y-0 space-x-2.5 flex-row items-center py-4 pl-3.5 pr-6">
             <Link href={href}>
@@ -45,6 +42,7 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
               {username}
             </Link>
           </DialogHeader>
+
           <ScrollArea className="hidden md:inline border-b flex-1 py-1.5">
             <MiniPost post={post} />
             {post.comments.length > 0 && (
@@ -61,10 +59,12 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
               </>
             )}
           </ScrollArea>
+
           <ViewPost className="hidden md:flex border-b" />
+
           <div className="px-2 hidden md:block mt-auto border-b p-2.5">
             <PostActions post={post} userId={user?.id} />
-            <time className="text-[11px] uppercase text-zinc-500">
+            <time className="text-[11px]  uppercase text-zinc-500 font-medium">
               {new Date(post.createdAt).toLocaleDateString("en-US", {
                 month: "long",
                 day: "numeric",
@@ -77,10 +77,8 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
             inputRef={inputRef}
           />
         </div>
-        <div
-          className="relative overflow-hidden h-96 md:h-[500px] lg:h-[700px] xl:h-[800px]
-        max-w-3xl w-full"
-        >
+
+        <div className="relative overflow-hidden h-96 md:h-[500px] lg:h-[700px] xl:h-[800px] max-w-3xl w-full">
           <Image
             src={post.fileUrl}
             fill
@@ -89,6 +87,7 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
             className="md:rounded-l-md object-cover"
           />
         </div>
+
         <PostActions
           post={post}
           userId={user?.id}
@@ -100,4 +99,5 @@ function PostView({ id, post }: { id: string; post: PostWithExtras }) {
     </Dialog>
   );
 }
+
 export default PostView;
